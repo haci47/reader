27b1f9cdb22835b5673b42645216562b421e1066
haci47 <ergulhaci339@gmail.com>
2021-02-18 17:18:21 +0200

    Initial commit

---
+++ src/com/erg/Main.java
@@ -0,0 +1,19 @@
+package com.erg;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi1 = 10;
+        int sayi2 = 20;
+
+        System.out.println("Sayılar : " + sayi1 + " " + sayi2);
+        int toplam = sayi1 + sayi2;
+
+        System.out.println("Toplam: " + toplam);
+
+    }
+}
+
+
+
+
